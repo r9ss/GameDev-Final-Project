e53e620ec9c270f30345b493551c16c51e14d067
r9ss <xfiynum1@gmail.com>
2022-08-02 02:41:55 +0300

    updated the gun shooting system!

---
+++ Project X/Assets/target.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+   public float health = 50f;
+
+   public void TakeDamage (float amount)
+{
+     health -= amount;
+     if (health <= 0f)
+     {
+         Die();
+     }
+}
+   void Die()
+   {
+        Destroy(gameObject);
+   }
+}
